2d2e8fdca3a468c965fee76a9eb4c866aebffdf1
Hiju <rose6649@likelion.org>
2022-06-30 00:55:53 +0900

    add: 프로젝트 생성

---
+++ SocketExample/SocketExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketExample
+//
+//  Created by hiju on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
